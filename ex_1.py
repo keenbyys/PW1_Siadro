ac172c0a6dfd52554141af86fe6abdfd9ec81803
keenbyys <166899952+keenbyys@users.noreply.github.com>
2025-09-18 10:40:11 +0300

    ex1

---
+++ ex_1.py
@@ -0,0 +1,53 @@
+character_level = 80
+print(f"Character Level: {character_level}")
+
+attack_stat = 1256.75
+print(f"Attack Stat: {attack_stat}")
+
+character_name = "Kafka"
+print(f"Character Name: {character_name}")
+
+team = ["Kafka", "Black Swan", "Tingyun", "Huohuo"]
+print(f"Team Composition: {team}")
+
+character_stats = {
+    "Name": "Kafka",
+    "HP": 4235.5,
+    "Element": "Lightning",
+    "Path": "Nihility"
+}
+print(f"Character Stats: {character_stats}")
+
+#######################################################################################
+
+# number = int(input("Enter a number: "))
+#
+# if number % 2 == 0:
+#     print(f"{number} is even")
+# else:
+#     print(f"{number} is odd")
+
+#######################################################################################
+
+# characters = ["March 7th", "Dan Heng", "Himeko", "Welt", "Kafka"]
+# for character in characters:
+#     print(f"Character: {character}")
+#
+# energy = 0
+# max_energy = 5
+# while energy < max_energy:
+#     print(f"Recharging {characters[0]}'s energy: {energy + 1}")
+#     energy += 1
+# print("Energy fully recharged!")
+
+#######################################################################################
+
+# def calculate_rectangle_area(length, width):
+#     area = length * width
+#     return f"The area of the rectangle with length {length} and width {width} is {area}"
+#
+# length = float(input("Enter the length of the rectangle: "))
+# width = float(input("Enter the width of the rectangle: "))
+#
+# result = calculate_rectangle_area(length, width)
+# print(result)
